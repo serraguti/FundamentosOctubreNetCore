5203f6b4a896589ef56a0c8eea9f3b17d3b646ef
serraguti <pacoserranox@gmail.com>
2024-10-28 19:28:23 +0100

    fdsfdfs

--- FundamentosOctubreNetCore/Form1.cs
+++ FundamentosOctubreNetCore/Form1.cs
@@ -12,6 +12,12 @@ namespace FundamentosOctubreNetCore
             //AQUI ES DONDE HAREMOS LAS ACCIONES CUANDO SE PULSE SOBRE EL BOTON
             //SOLAMENTE PODREMOS ESCRIBIR AQUI, ENTRE LAS LLAVES.
             //EN NINGUN OTRO SITIO
+            this.txtNombre.Text = "Soy un texto";
+            this.txtNombre.Width = 800;
+
+            this.txtNombre.Location = new Point(100, 100);
+
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
         }
     }
 }
