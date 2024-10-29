b5e4e0937698c62a56196f15bdcf6106b427d3d8
serraguti <pacoserranox@gmail.com>
2024-10-29 18:39:04 +0100

    dsasda

---
+++ FundamentosOctubreNetCore/Form05DiaNacimiento.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace FundamentosOctubreNetCore
+{
+    partial class Form05DiaNacimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtDia = new TextBox();
+            label2 = new Label();
+            txtMes = new TextBox();
+            label3 = new Label();
+            txtAnyo = new TextBox();
+            btnCalcularDiaNacimiento = new Button();
+            lblDiaSemana = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Día";
+            // 
+            // txtDia
+            // 
+            txtDia.Location = new Point(132, 28);
+            txtDia.Name = "txtDia";
+            txtDia.Size = new Size(100, 35);
+            txtDia.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Mes";
+            // 
+            // txtMes
+            // 
+            txtMes.Location = new Point(132, 86);
+            txtMes.Name = "txtMes";
+            txtMes.Size = new Size(100, 35);
+            txtMes.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 30);
+            label3.TabIndex = 4;
+            label3.Text = "Año";
+            // 
+            // txtAnyo
+            // 
+            txtAnyo.Location = new Point(132, 141);
+            txtAnyo.Name = "txtAnyo";
+            txtAnyo.Size = new Size(100, 35);
+            txtAnyo.TabIndex = 5;
+            // 
+            // btnCalcularDiaNacimiento
+            // 
+            btnCalcularDiaNacimiento.Location = new Point(279, 67);
+            btnCalcularDiaNacimiento.Name = "btnCalcularDiaNacimiento";
+            btnCalcularDiaNacimiento.Size = new Size(300, 64);
+            btnCalcularDiaNacimiento.TabIndex = 6;
+            btnCalcularDiaNacimiento.Text = "Calcular dia nacimiento";
+            btnCalcularDiaNacimiento.UseVisualStyleBackColor = true;
+            btnCalcularDiaNacimiento.Click += btnCalcularDiaNacimiento_Click;
+            // 
+            // lblDiaSemana
+            // 
+            lblDiaSemana.AutoSize = true;
+            lblDiaSemana.Location = new Point(47, 211);
+            lblDiaSemana.Name = "lblDiaSemana";
+            lblDiaSemana.Size = new Size(140, 30);
+            lblDiaSemana.TabIndex = 7;
+            lblDiaSemana.Text = "lblDiaSemana";
+            // 
+            // Form05DiaNacimiento
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(634, 276);
+            Controls.Add(lblDiaSemana);
+            Controls.Add(btnCalcularDiaNacimiento);
+            Controls.Add(txtAnyo);
+            Controls.Add(label3);
+            Controls.Add(txtMes);
+            Controls.Add(label2);
+            Controls.Add(txtDia);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form05DiaNacimiento";
+            Text = "Form05DiaNacimiento";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtDia;
+        private Label label2;
+        private TextBox txtMes;
+        private Label label3;
+        private TextBox txtAnyo;
+        private Button btnCalcularDiaNacimiento;
+        private Label lblDiaSemana;
+    }
+}
---
+++ FundamentosOctubreNetCore/Form05DiaNacimiento.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosOctubreNetCore
+{
+    public partial class Form05DiaNacimiento : Form
+    {
+        public Form05DiaNacimiento()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularDiaNacimiento_Click(object sender, EventArgs e)
+        {
+            int dia = int.Parse(this.txtDia.Text);
+            int mes = int.Parse(this.txtMes.Text);
+            int anyo = int.Parse(this.txtAnyo.Text);
+            //15/06/1997
+            if (mes == 1)
+            {
+                mes = 13;
+                anyo = anyo - 1;
+            }else if (mes == 2)
+            {
+                mes = 14;
+                anyo = anyo - 1;
+            }
+            //COMENZAMOS A REALIZAR LAS OPERACIONES MATEMATICAS
+            //Multiplicar el Mes más 1 por 3 y dividirlo entre 5
+            int op1 = ((mes + 1) * 3) / 5;
+            //Dividir el año entre 4
+            int op2 = anyo / 4;
+            //Dividir el año entre 100
+            int op3 = anyo / 100;
+            //Dividir el año entre 400
+            int op4 = anyo / 400;
+            //Sumar el día, el doble del mes, el año, el resultado de la operación 1
+            //, el resultado de la operación 2, menos el resultado de la operación 3
+            //más la operación 4 más 2.
+            int op5 = dia + (mes * 2) + anyo + op1 + op2 - op3 + op4 + 2;
+            //Dividir el resultado anterior entre 7.
+            int op6 = op5 / 7;
+            //Restar el número del paso 5 con el número del paso 6 por 7.
+            int resultado = op5 - (op6 * 7);
+            if (resultado == 0)
+            {
+                this.lblDiaSemana.Text = "SABADO";
+            }else if (resultado == 1)
+            {
+                this.lblDiaSemana.Text = "DOMINGO";
+            }else if (resultado == 2)
+            {
+                this.lblDiaSemana.Text = "LUNES";
+            }else if (resultado == 3)
+            {
+                this.lblDiaSemana.Text = "MARTES";
+            }else if (resultado == 4)
+            {
+                this.lblDiaSemana.Text = "MIERCOLES";
+            }else if (resultado == 5)
+            {
+                this.lblDiaSemana.Text = "JUEVES";
+            }else if (resultado == 6)
+            {
+                this.lblDiaSemana.Text = "VIERNES";
+            }
+            else
+            {
+                this.lblDiaSemana.Text = "Tenemos un error, Houston";
+            }
+        }
+    }
+}
