b520e2e2dc942a9ff2e15297786454319099b42d
serraguti <pacoserranox@gmail.com>
2024-10-29 20:59:00 +0100

    fsrdfed

---
+++ FundamentosOctubreNetCore/Form07Bucles.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FundamentosOctubreNetCore
+{
+    partial class Form07Bucles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEjemploFor = new Button();
+            btnEjemploWhile = new Button();
+            txtResultado = new TextBox();
+            SuspendLayout();
+            // 
+            // btnEjemploFor
+            // 
+            btnEjemploFor.Location = new Point(34, 39);
+            btnEjemploFor.Name = "btnEjemploFor";
+            btnEjemploFor.Size = new Size(201, 76);
+            btnEjemploFor.TabIndex = 0;
+            btnEjemploFor.Text = "Ejemplo For";
+            btnEjemploFor.UseVisualStyleBackColor = true;
+            btnEjemploFor.Click += btnEjemploFor_Click;
+            // 
+            // btnEjemploWhile
+            // 
+            btnEjemploWhile.Location = new Point(303, 40);
+            btnEjemploWhile.Name = "btnEjemploWhile";
+            btnEjemploWhile.Size = new Size(214, 75);
+            btnEjemploWhile.TabIndex = 1;
+            btnEjemploWhile.Text = "Ejemplo While";
+            btnEjemploWhile.UseVisualStyleBackColor = true;
+            btnEjemploWhile.Click += btnEjemploWhile_Click;
+            // 
+            // txtResultado
+            // 
+            txtResultado.Location = new Point(38, 145);
+            txtResultado.Multiline = true;
+            txtResultado.Name = "txtResultado";
+            txtResultado.Size = new Size(479, 113);
+            txtResultado.TabIndex = 2;
+            // 
+            // Form07Bucles
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(561, 324);
+            Controls.Add(txtResultado);
+            Controls.Add(btnEjemploWhile);
+            Controls.Add(btnEjemploFor);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form07Bucles";
+            Text = "Form07Bucles";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEjemploFor;
+        private Button btnEjemploWhile;
+        private TextBox txtResultado;
+    }
+}
---
+++ FundamentosOctubreNetCore/Form07Bucles.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosOctubreNetCore
+{
+    public partial class Form07Bucles : Form
+    {
+        public Form07Bucles()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjemploFor_Click(object sender, EventArgs e)
+        {
+            //EN UN BUCLE FOR, DEBEMOS INDICAR UNA VARIABLE QUE SERA 
+            //EL INICIO.  DICHA VARIABLE, SOLAMENTE LA PODEMOS UTILIZAR
+            //DENTRO DEL BUCLE { ... }
+            //HACEMOS UN BUCLE DE 1 A 7
+            string resultado = "";
+            for (int i = 1; i <= 7; i++)
+            {
+                resultado = resultado + i + ", ";
+                //resultado += i + ", ";
+            }
+            this.txtResultado.Text = resultado;
+        }
+
+        private void btnEjemploWhile_Click(object sender, EventArgs e)
+        {
+            string resultado = "";
+            int contador = 1;
+            while (contador <= 7)
+            {
+                resultado += contador + ", ";
+                //DEBEMOS HACER QUE SALGA DEL BUCLE ALGUNA VEZ
+                contador += 1;
+            }
+            this.txtResultado.Text = resultado;
+            //PODEMOS REALIZAR LO MISMO EN UN BUCLE WHILE QUE EN UN FOR
+            //POR EJEMPLO, EL CODIGO QUE HEMOS UTILIZADO DE 1 - 7
+            //LOS BUCLES WHILE FUNCIONAN CON UNA CONDICION
+            //NECESITAMOS "ALGO" QUE INDIQUE NUESTRA CONDICION
+            //Y, DENTRO DEL BUCLE, DEBEMOS CAMBIAR DICHA CONDICION
+
+
+            //bool respuesta = true;
+            //while (respuesta == true)
+            //{
+            //    //SE VA A QUEDAR AQUI DENTRO
+            //    this.txtResultado.AppendText("@");
+            //    //DEBEMOS CAMBIAR LA CONDICION 
+            //    //SI LA LONGITUD DEL TEXTO DE LA CAJA LLEGA A 100
+            //    //DENTRO DE .Text DE UNA CAJA TENEMOS UNA PROPIEDAD LLAMADA 
+            //    //.TextLength QUE NOS INDICA LA LONGITUD DE UN TEXTO
+            //    int longitud = this.txtResultado.TextLength;
+            //    if (longitud == 100)
+            //    {
+            //        respuesta = false;
+            //    }
+            //}
+        }
+    }
+}
