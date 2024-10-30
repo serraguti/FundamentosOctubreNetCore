6f442bb04f9b35c08b0d592a5316b7e596234b32
serraguti <pacoserranox@gmail.com>
2024-10-30 18:55:57 +0100

    fdsfesfgdgfd

--- FundamentosOctubreNetCore/Form12ValidarEmail.cs
+++ FundamentosOctubreNetCore/Form12ValidarEmail.cs
@@ -24,12 +24,39 @@ namespace FundamentosOctubreNetCore
             if (email.Contains("@") == false)
             {
                 this.lblResultado.Text = "No existe @ en el mail";
+            }else if (email.IndexOf(".") == -1)
+            {
+                this.lblResultado.Text = "Debe existir un punto";
+            }else if (email.StartsWith("@") == true)
+            {
+                this.lblResultado.Text = "@ al inicio del mail";
+            }else if (email.EndsWith("@") == true)
+            {
+                this.lblResultado.Text = "@ al final del email";
+            }else if (email.IndexOf("@") != email.LastIndexOf("@"))
+            {
+                this.lblResultado.Text = "Existe más de una @ en el mail";
+            }
+            else if (email.LastIndexOf(".") < email.IndexOf("@"))
+            {
+                this.lblResultado.Text = "Debemos tener un punto despues de la @";
             }
             else
             {
-                this.lblResultado.Text = "Mail correcto";
+                //CAPTURAMOS LA POSICION DEL ULTIMO PUNTO
+                int ultimoPunto = email.LastIndexOf(".");
+                //DEBEMOS QUEDARNOS CON EL DOMINIO, PERO SIN EL PUNTO
+                //email@dominio.com
+                string dominio = email.Substring(ultimoPunto + 1);
+                if (dominio.Length >= 2 && dominio.Length <= 4)
+                {
+                    this.lblResultado.Text = "Mail correcto";
+                }
+                else
+                {
+                    this.lblResultado.Text = "El dominio debe ser de 2 a 4 caracteres";
+                }
             }
-            //VUESTROS IF A PARTIR DE AQUI
         }
     }
 }
