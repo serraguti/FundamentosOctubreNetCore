5efa6a4fc503ca8473b9417835096379486401c5
serraguti <pacoserranox@gmail.com>
2024-11-04 17:55:56 +0100

    fdsfdsf

--- ProyectoClases/Persona.cs
+++ ProyectoClases/Persona.cs
@@ -11,6 +11,33 @@ namespace ProyectoClases
         //VAMOS A CREAR TRES PROPIEDADES
         public string Nombre;
         public string Apellidos;
-        public int Edad;
+
+        private int _CampoEdad;
+
+        public int Edad
+        {
+            get {
+                //CUANDO UN USUARIO QUIERA EL VALOR, LE DEVOLVEMOS EL CAMPO
+                return this._CampoEdad;
+            }
+            set { 
+                //CUANDO UN USUARIO QUIERA ESTABLECER UN VALOR, PRIMERO
+                //COMPROBAMOS
+                if (value < 0)
+                {
+                    //NO ME GUSTA, PUES LANZO UN ERROR DE PROGRAMA
+                    throw new Exception("La edad no puede ser negativa");
+                }
+                else
+                {
+                    //SI NOS GUSTA, ALMACENAMOS EL DATO EN EL CAMPO
+                    this._CampoEdad = value;
+                }
+            }
+        }
+
+
+
+
     }
 }
