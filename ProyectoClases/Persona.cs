f848acc597f8cd54f3d4d47d9c43d113393bd6e1
serraguti <pacoserranox@gmail.com>
2024-11-04 17:36:01 +0100

    fdsfdsfs

---
+++ ProyectoClases/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClases
+{
+    public class Persona
+    {
+        //VAMOS A CREAR TRES PROPIEDADES
+        public string Nombre;
+        public string Apellidos;
+        public int Edad;
+    }
+}
