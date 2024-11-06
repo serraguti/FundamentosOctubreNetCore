48ee8969d288ab9119f04d92cadb97fa8e315286
serraguti <pacoserranox@gmail.com>
2024-11-06 17:59:43 +0100

    dsadsadsa

---
+++ AdoNet/Form04EliminarEnfermos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace AdoNet
+{
+    public partial class Form04EliminarEnfermos : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form04EliminarEnfermos()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            string sql = "select * from ENFERMO";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstEnfermos.Items.Clear();
+            while (this.reader.Read())
+            {
+                string inscripcion = this.reader["INSCRIPCION"].ToString();
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.lstEnfermos.Items.Add(inscripcion + " - " + apellido);
+            }
+            this.cn.Close();
+            this.reader.Close();
+        }
+    }
+}
