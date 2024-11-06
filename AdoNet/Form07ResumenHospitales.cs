d6eb5254705e1073d270692193072b5ee6516bcd
serraguti <pacoserranox@gmail.com>
2024-11-06 19:42:54 +0100

    cdzxcxzc

--- AdoNet/Form07ResumenHospitales.cs
+++ AdoNet/Form07ResumenHospitales.cs
@@ -41,7 +41,34 @@ namespace AdoNet
 
         private void lstHospitales_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            string sql = "select COUNT(*) as PERSONAS, MAX(DOCTOR.SALARIO) as MAXIMOSALARIO "
+                + ", MIN(DOCTOR.SALARIO) AS MINIMOSALARIO"
+                + ", HOSPITAL.NOMBRE "
+                + " from DOCTOR "
+                + " inner join HOSPITAL "
+                + " on DOCTOR.HOSPITAL_COD = HOSPITAL.HOSPITAL_COD "
+                + " where HOSPITAL.NOMBRE=@nombre "
+                + " group by HOSPITAL.NOMBRE";
+            string nombre = this.lstHospitales.SelectedItem.ToString();
+            SqlParameter pamNombre = new SqlParameter("@nombre", nombre);
+            this.com.Parameters.Add(pamNombre);
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            while (this.reader.Read())
+            {
+                string personas = this.reader["PERSONAS"].ToString();
+                string maximoSalario = this.reader["MAXIMOSALARIO"].ToString();
+                string minimoSalario = this.reader["MINIMOSALARIO"].ToString();
+                this.txtDoctores.Text = personas;
+                this.txtMaximoSalario.Text = maximoSalario;
+                this.txtMinimoSalario.Text = minimoSalario;
+            }
+            this.reader.Close();
+            this.cn.Close();
+            this.com.Parameters.Clear();
         }
     }
 }
