cfd9346b69e23da053db58117ad27fea6e75d55a
serraguti <pacoserranox@gmail.com>
2024-11-07 17:30:35 +0100

    fdsfds

---
+++ AdoNet/Form10DepartamentosListView.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AdoNet
+{
+    partial class Form10DepartamentosListView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lsvDepartamentos = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Departamentos";
+            // 
+            // lsvDepartamentos
+            // 
+            lsvDepartamentos.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            lsvDepartamentos.FullRowSelect = true;
+            lsvDepartamentos.Location = new Point(29, 52);
+            lsvDepartamentos.Name = "lsvDepartamentos";
+            lsvDepartamentos.Size = new Size(538, 402);
+            lsvDepartamentos.TabIndex = 1;
+            lsvDepartamentos.UseCompatibleStateImageBehavior = false;
+            lsvDepartamentos.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "ID";
+            columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "NOMBRE";
+            columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "LOCALIDAD";
+            columnHeader3.Width = 180;
+            // 
+            // Form10DepartamentosListView
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(598, 482);
+            Controls.Add(lsvDepartamentos);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form10DepartamentosListView";
+            Text = "Form10DepartamentosListView";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListView lsvDepartamentos;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+    }
+}
---
+++ AdoNet/Form10DepartamentosListView.cs
@@ -0,0 +1,44 @@
+﻿using AdoNet.Models;
+using AdoNet.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet
+{
+    public partial class Form10DepartamentosListView : Form
+    {
+        RepositoryDepartamentos repo;
+
+        public Form10DepartamentosListView()
+        {
+            InitializeComponent();
+            this.repo = new RepositoryDepartamentos();
+            this.CargarDepartamentos();
+        }
+
+        private void CargarDepartamentos()
+        {
+            List<Departamento> departamentos = this.repo.GetDepartamentos();
+            //RECORREMOS TODOS LOS DEPARTAMENTOS PARA EL DIBUJO
+            foreach (Departamento dept in departamentos)
+            {
+                //POR CADA DEPARTAMENTO, QUEREMOS UNA FILA EN EL DIBUJO
+                ListViewItem item = new ListViewItem();
+                //EN LA PRIMERA COLUMNA DIBUJAMOS EL NUMERO DEL DEPARTAMENTO
+                item.Text = dept.IdDepartamento.ToString();
+                //EL RESTO DE COLUMNAS EN EL ORDEN QUE HAYAMOS REALIZADO EL DIBUJO
+                item.SubItems.Add(dept.Nombre);
+                item.SubItems.Add(dept.Localidad);
+                //AGREGAMOS CADA ITEM AL CONTROL LISTVIEW
+                this.lsvDepartamentos.Items.Add(item);
+            }
+        }
+    }
+}
