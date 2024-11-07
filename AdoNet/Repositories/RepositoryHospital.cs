c5ae282d735e39794ea0a72ab4ecdf9edbc7b523
serraguti <pacoserranox@gmail.com>
2024-11-07 17:52:41 +0100

    fdsfdfs

---
+++ AdoNet/Repositories/RepositoryHospital.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using AdoNet.Models;
+
+namespace AdoNet.Repositories
+{
+    public class RepositoryHospital
+    {
+        private SqlConnection cn;
+        private SqlCommand com;
+        private SqlDataReader reader;
+
+        public RepositoryHospital()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.com.CommandType = System.Data.CommandType.Text;
+        }
+
+        //METODO PARA RECUPERAR TODOS LOS HOSPITALES List<Hospital>
+        public List<Hospital> GetHospitales()
+        {
+            string sql = "select * from HOSPITAL";
+            this.com.CommandText = sql;
+
+        }
+    }
+}
