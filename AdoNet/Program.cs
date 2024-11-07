64e18c3e80eedb2518d584874acff5ed9155b3da
serraguti <pacoserranox@gmail.com>
2024-11-07 20:49:23 +0100

    cszdsda

--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -13,7 +13,7 @@ namespace AdoNet
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form12Settings());
+            Application.Run(new Form10DepartamentosListView());
         }
 
         public static IConfigurationRoot GetConfiguration()
