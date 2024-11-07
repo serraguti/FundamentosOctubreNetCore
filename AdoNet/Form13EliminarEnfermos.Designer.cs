a6c10440ed18a015762113a93ce9823f015c5674
serraguti <pacoserranox@gmail.com>
2024-11-07 20:59:26 +0100

    gfdgfd

---
+++ AdoNet/Form13EliminarEnfermos.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AdoNet
+{
+    partial class Form13EliminarEnfermos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lsvEnfermos = new ListView();
+            btnEliminarEnfermo = new Button();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Enfermos";
+            // 
+            // lsvEnfermos
+            // 
+            lsvEnfermos.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6 });
+            lsvEnfermos.FullRowSelect = true;
+            lsvEnfermos.Location = new Point(27, 48);
+            lsvEnfermos.Name = "lsvEnfermos";
+            lsvEnfermos.Size = new Size(798, 306);
+            lsvEnfermos.TabIndex = 1;
+            lsvEnfermos.UseCompatibleStateImageBehavior = false;
+            lsvEnfermos.View = View.Details;
+            // 
+            // btnEliminarEnfermo
+            // 
+            btnEliminarEnfermo.BackColor = Color.Red;
+            btnEliminarEnfermo.ForeColor = Color.White;
+            btnEliminarEnfermo.Location = new Point(27, 369);
+            btnEliminarEnfermo.Name = "btnEliminarEnfermo";
+            btnEliminarEnfermo.Size = new Size(223, 65);
+            btnEliminarEnfermo.TabIndex = 2;
+            btnEliminarEnfermo.Text = "Eliminar enfermo";
+            btnEliminarEnfermo.UseVisualStyleBackColor = false;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Inscripción";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Apellido";
+            columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Dirección";
+            columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Fecha nacimiento";
+            columnHeader4.Width = 130;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Sexo";
+            columnHeader5.Width = 70;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "NSS";
+            columnHeader6.Width = 150;
+            // 
+            // Form13EliminarEnfermos
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(925, 503);
+            Controls.Add(btnEliminarEnfermo);
+            Controls.Add(lsvEnfermos);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form13EliminarEnfermos";
+            Text = "Form13EliminarEnfermos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListView lsvEnfermos;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private Button btnEliminarEnfermo;
+    }
+}
---
+++ AdoNet/Form13EliminarEnfermos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet
+{
+    public partial class Form13EliminarEnfermos : Form
+    {
+        public Form13EliminarEnfermos()
+        {
+            InitializeComponent();
+        }
+    }
+}
