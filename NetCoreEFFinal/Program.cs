08554a75cb4ff5643d86ca9b927ba6088569e61d
serraguti <pacoserranox@gmail.com>
2024-11-13 18:02:51 +0100

    fdsfdsf

--- NetCoreEFFinal/Program.cs
+++ NetCoreEFFinal/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreEFFinal.Data;
+using NetCoreEFFinal.Repositories;
+
 namespace NetCoreEFFinal
 {
     internal static class Program
     {
+        public static ServiceProvider Provider;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,6 +18,22 @@ namespace NetCoreEFFinal
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            //RECUPERAMOS LA CADENA DE CONEXION DEL FICHERO DE CONFIGURACION
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            IConfigurationRoot configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("HospitalSQLServer");
+
+            //INYECTAMOS LAS CLASES NECESARIAS PARA LOS FORMULARIOS Y TAMBIEN PARA
+            //ENTITY FRAMEWORK
+            Provider = new ServiceCollection()
+                .AddTransient<RepositoryHospitales>()
+                .AddDbContext<HospitalContext>
+                (options => options.UseSqlServer(connectionString))
+                .BuildServiceProvider();
+
             Application.Run(new Form1());
         }
     }
