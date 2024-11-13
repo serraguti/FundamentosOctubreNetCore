b66a10fa54906a74adaa58f317511bfea351169e
serraguti <pacoserranox@gmail.com>
2024-11-13 18:10:07 +0100

    fsdafasfsa

---
+++ NetCoreEFFinal/Form01CrudHospitales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetCoreEFFinal
+{
+    public partial class Form01CrudHospitales : Form
+    {
+        public Form01CrudHospitales()
+        {
+            InitializeComponent();
+        }
+    }
+}
