772ae919b2003663f16fdec24a9c1bd84444a6a8
serraguti <pacoserranox@gmail.com>
2024-11-14 18:09:06 +0100

    dsadsadsa

---
+++ ServiciosApiCliente/Form01Doctores.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ServiciosApiCliente
+{
+    partial class Form01Doctores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lsvDoctores = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Doctores";
+            // 
+            // lsvDoctores
+            // 
+            lsvDoctores.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            lsvDoctores.FullRowSelect = true;
+            lsvDoctores.Location = new Point(28, 55);
+            lsvDoctores.Name = "lsvDoctores";
+            lsvDoctores.Size = new Size(803, 366);
+            lsvDoctores.TabIndex = 1;
+            lsvDoctores.UseCompatibleStateImageBehavior = false;
+            lsvDoctores.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Apellido";
+            columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Especialidad";
+            columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Salario";
+            columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Hospital";
+            columnHeader5.Width = 120;
+            // 
+            // Form01Doctores
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1030, 475);
+            Controls.Add(lsvDoctores);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form01Doctores";
+            Text = "Form01Doctores";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListView lsvDoctores;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+    }
+}
---
+++ ServiciosApiCliente/Form01Doctores.cs
@@ -0,0 +1,42 @@
+﻿using ServiciosApiCliente.Models;
+using ServiciosApiCliente.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiciosApiCliente
+{
+    public partial class Form01Doctores : Form
+    {
+        ServiceDoctores service;
+
+        public Form01Doctores()
+        {
+            InitializeComponent();
+            this.service = new ServiceDoctores();
+            this.CargarDoctoresAsync();
+        }
+
+        private async Task CargarDoctoresAsync()
+        {
+            List<Doctor> doctores = await this.service.GetDoctoresAsync();
+            this.lsvDoctores.Items.Clear();
+            foreach (Doctor doc in doctores) 
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = doc.IdDoctor.ToString();
+                item.SubItems.Add(doc.Apellido);
+                item.SubItems.Add(doc.Especialidad);
+                item.SubItems.Add(doc.Salario.ToString());
+                item.SubItems.Add(doc.IdHospital.ToString());
+                this.lsvDoctores.Items.Add(item);
+            }
+        }
+    }
+}
